fca4e616b4383e4bc3611fb78ef6ee577228a021
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-02-10 19:43:21 +0300

    Added solving a problem about two friends & dog

---
+++ TwoFriends/Program.cs
@@ -0,0 +1,22 @@
+﻿int firstFriendSpeed = 1;
+int secondFriendSpeed = 3;
+int dogSpeed = 5;
+int initialDistance = 1000;
+int finalDistance = 10;
+int friend = 0;
+int count = 0;
+int time = 0;
+
+while (initialDistance > finalDistance) {
+    if (friend % 2 == 0) {
+        time = initialDistance / (secondFriendSpeed + dogSpeed);
+    } else {
+        time = initialDistance / (firstFriendSpeed + dogSpeed);
+    }
+    initialDistance = initialDistance - time * (firstFriendSpeed + secondFriendSpeed);
+    friend++;
+    count++;
+    Console.WriteLine(initialDistance);
+}
+Console.Write ("Количество пробежек собаки равно: ");
+Console.WriteLine (count);
