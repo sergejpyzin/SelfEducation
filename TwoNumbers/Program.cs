d7c2ac77f4b45d9950c2b59845202f8449560dfa
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-02-11 16:28:08 +0300

    Created TwoNumbers directory

---
+++ TwoNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿// Запрашиваем данные у пользователя
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// Проверяем условия задачи и выводим решение
+if (number >= 5 && number <= 10)
+{
+    System.Console.WriteLine("Число " + number + " находится в интервале от 5 до 10");
+}
+else
+{
+    System.Console.Write("Число " + number + " неивестно. Попробуйте еще раз");
+}
+
+
