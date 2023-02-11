d98522241f9b3b1f669e6d34db7f2af0a1c25fa4
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-02-11 19:48:36 +0300

    Created Sorting directory

---
+++ Sorting/Program.cs
@@ -0,0 +1,16 @@
+﻿// Заполняем массив
+int [] array = new int [] {1, 3, 6, 8, 2, 5};
+
+/* for (int i = 0; i < array.Length; i++)
+{
+    System.Console.Write("Введите целое число: ");
+    int arrayNumber = Convert.ToInt32(Console.ReadLine());
+}
+System.Console.WriteLine("Массив чмсел: ");
+for (int i = 0; i < number; i++)
+{
+    System.Console.Write(array[i]);
+   
+} */
+
+
