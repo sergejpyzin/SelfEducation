4089e04e41c50dbcca1308f67b732a59499f4bc2
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-02-13 18:07:51 +0300

    Created SunTwoNumber folder

---
+++ SumTwoNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу на C # Sharp для вычисления суммы двух заданных целочисленных значений. 
+// Если эти два значения одинаковы, вернуть тройную сумму.
+
+// Запрашиваем информацию у пользователя
+System.Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+// Вводим новую перменную  и производим основное вычисление
+int sum = numberA + numberB;
+
+// Проверяем условия задачи
+if (numberA == numberB) sum = 3 * sum;
+
+// Выводим результат
+System.Console.WriteLine(sum);
