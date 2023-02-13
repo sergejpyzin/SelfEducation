25107c79703296a42213d2825cb1159bc96dcca6
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-02-13 18:46:39 +0300

    Saving all change

---
+++ BooleanOutput/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу на C # Sharp для проверки двух заданных целых чисел и 
+// верните true, если одному из них будет 30 или если их сумма равна 30.
+
+// Запрашиваем информацию у пользователя
+System.Console.Write("Введите превое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+// Проводим вычисления
+int result = numberA + numberB;
+
+// Проверяем условия и выводим результат
+if (numberA == 30 || numberB == 30 || result == 30)
+{
+    System.Console.WriteLine(true);
+}
+else
+{
+    System.Console.WriteLine(false);
+}
--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -6,7 +6,7 @@ int [] array = new int [] {1, 3, 6, 8, 2, 5};
     System.Console.Write("Введите целое число: ");
     int arrayNumber = Convert.ToInt32(Console.ReadLine());
 }
-System.Console.WriteLine("Массив чмсел: ");
+System.Console.WriteLine("Массив чbсел: ");
 for (int i = 0; i < number; i++)
 {
     System.Console.Write(array[i]);
